aca5fdf9bb93709c7b6d276c3fac36aa3d922879
Mariano Rigo <marianowagnerrigo@gmail.com>
2022-08-19 18:47:30 -0300

    primer commit en la rama menu

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const listElements = document.querySelectorAll(".list__bottom--click");
 
+const iconMenu = document.querySelector(".icon__menu");
+
+const navMenu = document.querySelector(".list");
+
+iconMenu.addEventListener("click", ()=>{
+    navMenu.classList.toggle("visible");
+})
+
 listElements.forEach(listElements =>{
     listElements.addEventListener("click", ()=>{
         listElements.classList.toggle("arrow")
